a615d7896a181c093c4f505b13b7a8d40401f9a9
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-03 16:23:48 -0500

    Starting work on controllers

---
+++ WebAPI/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(MyAppContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ WebAPI/Data/MyAppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+
+
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Painting> Paintings { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
